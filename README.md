073eada63bf0e7357d0a9e1fadfee057cc29c680
Max Scher and Nathan Stewart <max@>
2017-06-07 09:05:33 -0700

    return false for any input

---
+++ README.md
@@ -0,0 +1,6 @@
+# Leap Year Machine #
+
+
+specs:
+-return true/false when a year is submitted.
+-
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 //put your biz-logic here:
 var leapYear = function(year) {
-
-}
+  return false;
+};
 
 
 
 //put your UI logic here:
 $(document).ready(function() {
-  $(".machine").submit(function(event) {
+  $("form#machine").submit(function(event) {
     event.preventDefault();
 
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $(".result").text(result);
+        console.log(result);
+    $("#result").text(result);
   });
 });
