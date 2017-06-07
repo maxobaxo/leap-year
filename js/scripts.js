8b98b633be7b066ee68f8a18b16d0fd6d5871277
Max Scher and Nathan Stewart <max@>
2017-06-07 09:12:58 -0700

    combine both true conditions in one statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 //put your biz-logic here:
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
   } else {
     return false;
