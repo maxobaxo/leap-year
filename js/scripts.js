edfdc5b387033b4adb289188f4d8b7e71b8042f8
Max Scher and Nathan Stewart <nathan@>
2017-06-07 09:16:40 -0700

    return true if input divisibkle by 400

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //put your biz-logic here:
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
